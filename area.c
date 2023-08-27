e6b026293d261249259af466486cd9e2c3c1fcb0
Pankaj Ashok Dhavale <143345046+pankaj6994@users.noreply.github.com>
2023-08-27 22:33:25 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main(){
+int a,b,c;
+a=10;
+b=20;
+c=a+b;
+printf("Addition is %d",c);}*/
+
+//Area of circle
+#include<stdio.h>
+void main(){
+int r=2;
+float pi=3.14,a;
+a=pi*r*r;
+printf("Area of circle is %f",a);}
+
+//area of rectangle
+#include<stdio.h>
+void main(){
+int l=10, b=20, c;
+c=l*b;
+printf("Area of Rectangle is %d",c);}
+
+//area of triangle
+#include<stdio.h>
+void main(){
+int b=2, h=4, a;
+a=0.5*b*h;
+printf("Area of Triangle is %d",a);}
+
+
+//Area of square
+#include<stdio.h>
+void main(){
+int s=4, o;
+o=s*s;
+printf("Area of square is %d\n",o);}
